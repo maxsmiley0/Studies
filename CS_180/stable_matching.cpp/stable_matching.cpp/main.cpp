3f8f58b3a9b482f8a93c29ca76fbeeefe33c0164
Max Smiley <maxsmiley0@gmail.com>
2021-04-26 13:37:42 -0700

    Work in CS180, started adding algorithmic programming exercises

---
+++ CS_180/stable_matching.cpp/stable_matching.cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  stable_matching.cpp
+//
+//  Created by Max Smiley on 4/26/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
